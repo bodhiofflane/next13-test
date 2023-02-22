f6f4a87e91af7b15f86753c752218eb17be8fd0a
bodhiofflane <channelbodhi@gmail.com>
2023-02-22 19:55:39 +0300

    grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,14 +1,17 @@
 import cn from 'classnames';
+import {times} from 'data-fns'
 
 import { FooterProps } from './Footer.props';
 
 import styles from './Footer.module.css';
 
-const Footer = ({...props}: FooterProps) => {
+const Footer = ({className, ...props}: FooterProps) => {
   return (
-    <div {...props}>
-      Footer
-    </div>
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>Все права защищены. 2020 - 2023</div>
+      <a href="#" target="_blank">Пользовательское соглашение</a>
+      <a href="#">Политика конф.</a>
+    </footer>
   );
 }
  
